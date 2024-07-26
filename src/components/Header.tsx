5e2296fcf391240e79caf688106f94497158d1c9
Md Mustaqim Alam <mustaqimalam85@gmail.com>
2024-07-26 22:53:17 +0530

    NavBar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { FaShoppingBag, FaSignInAlt } from "react-icons/fa";
+import { HiSearch } from "react-icons/hi";
+import { PiSignInBold, PiSignInFill } from "react-icons/pi";
+import { VscSignIn } from "react-icons/vsc";
+import { Link } from "react-router-dom";
 
+const user = { _id: "", role: "user" };
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/search">{<HiSearch />} </Link>
+        <Link to="/cart">{<FaShoppingBag />}</Link>
+        {user?._id ? (
+          <></>
+        ) : (
+          <Link to="/login">
+            SignIn
+          </Link>
+        )}
+      </nav>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
