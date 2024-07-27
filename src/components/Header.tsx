611d5e8895ebf5a381449e3fa887edc53b85eed3
Md Mustaqim Alam <mustaqimalam85@gmail.com>
2024-07-27 08:34:44 +0530

    NavBar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,39 @@
-import { FaShoppingBag, FaSignInAlt } from "react-icons/fa";
+import { useState } from "react";
+import { FaShoppingBag, FaUserAlt } from "react-icons/fa";
+import { GoSignOut } from "react-icons/go";
 import { HiSearch } from "react-icons/hi";
-import { PiSignInBold, PiSignInFill } from "react-icons/pi";
-import { VscSignIn } from "react-icons/vsc";
 import { Link } from "react-router-dom";
 
-const user = { _id: "", role: "user" };
+
+const user = { _id: "hgfdsgh", role: "user" };
 const Header = () => {
+  const [isOpen, setisOpen] = useState<boolean>(false);
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/search">{<HiSearch />} </Link>
-        <Link to="/cart">{<FaShoppingBag />}</Link>
-        {user?._id ? (
-          <></>
-        ) : (
-          <Link to="/login">
-            SignIn
-          </Link>
-        )}
-      </nav>
-    </div>
+    <nav className="header">
+      <Link to="/">Home</Link>
+      <Link to="/search">{<HiSearch />} </Link>
+      <Link to="/cart">{<FaShoppingBag />}</Link>
+      {user?._id ? (
+        <>
+          <button onClick={() => setisOpen((prev) => !prev)}>
+            <FaUserAlt />
+          </button>
+          <dialog open={isOpen}>
+            <div>
+              {user.role === "admin" && (
+                <button>
+                  <Link to="/admin/dashboard">Admin</Link>
+                </button>
+              )}
+              <Link to="/orders">Orders</Link>
+              <button>{<GoSignOut />}</button>
+            </div>
+          </dialog>
+        </>
+      ) : (
+        <Link to="/login">SignIn</Link>
+      )}
+    </nav>
   );
 };
 
